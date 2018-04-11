4512fcbb702b549f4e79036d8b391ebc576d926e
brenninger01 <bballer11241@gmail.com>
2018-04-11 15:28:24 -0500

    Added python for motion logs

---
+++ python/pan_sh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Edward C. Champion
+# 4/7/2108
+# Create while loop
+while true; do
+# Call hor.sh shell script
+	hor.sh 0
+# Sleep for 15 sec.
+# Call shell script and put in degrees
+	hor.sh 10
+	hor.sh 20
+	hor.sh 30
+	hor.sh 40
+	hor.sh 50
+	hor.sh 60
+	hor.sh 70
+	hor.sh 80
+	hor.sh 90
+	hor.sh 80
+	hor.sh 70
+	hor.sh 60
+	hor.sh 50
+	hor.sh 40
+	hor.sh 30
+	hor.sh 20
+	hor.sh 10
+	hor.sh 0
+
+
+done
+#sudo python hor.sh
