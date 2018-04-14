206726cff8fdf5eee737c557984ff737d8b8a4ec
brenninger01 <bballer11241@gmail.com>
2018-04-14 13:41:17 -0500

    added arming.php

--- js/homesecure.js
+++ js/homesecure.js
@@ -6,12 +6,12 @@ var accentColor1 = "#F0E100";
 var accentColor2 = "#EEBA0B";
 
 var themeSrc = "css/style.css";
-var camUrl = "http://172.19.20.41:8081/";
+//var camUrl = "http://172.19.20.41:8081/";
 //var camUrl = "https://wztxmxwb.p19.rt3.io/"
-//var camUrl = "http://192.168.0.123:8081"
+var camUrl = "http://192.168.0.123:8081"
 
-//var url = "http://192.168.0.123:8123/api/states"
-var url = "http://172.19.20.41:8123/api/states"
+var url = "http://192.168.0.123:8123/api/states"
+//var url = "http://172.19.20.41:8123/api/states"
 //var url = "https://rzyygcpm.p19.rt3.io/api/states"
 var firstLoad = 1;
 var gotten = 0;
