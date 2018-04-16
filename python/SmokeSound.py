4799cddaa20829c1d17c1072d89bbe949ab8f852
brenninger01 <bballer11241@gmail.com>
2018-04-16 14:34:58 -0500

    added sounds

---
+++ python/SmokeSound.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.5
+
+import pygame
+import time
+
+pygame.init()
+pygame.mixer.init()
+mysound = pygame.mixer.Sound("/var/www/html/sounds/SmokeAlarm.wav")
+mysound.play()
+time.sleep(5)
+mysound.stop()
+
