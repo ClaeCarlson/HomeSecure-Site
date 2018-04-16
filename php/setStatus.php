89716ac397a19a467980eeda3a2f50c05921a547
ClaeCarlson <ccarlson04@aurora.edu>
2018-04-16 12:30:01 -0500

    add session_start() fix to setStatus, debug alert for func

--- php/setStatus.php
+++ php/setStatus.php
@@ -1,13 +1,17 @@
 <?php
 
 require 'header.php';
+session_start();
 
 $status = $_POST['status'];
-
 $user = $_SESSION['username'];
 
+
 $result = $mysqli->query("UPDATE system SET status = '$status' WHERE id=1");
+
+
 $userResult = $mysqli->query("SELECT id FROM users WHERE username='$user'");
+
 $mysqli->query("INSERT INTO logs (status, user_id, system_id) VALUES('$status', '$userResult', 1)");
 
 
