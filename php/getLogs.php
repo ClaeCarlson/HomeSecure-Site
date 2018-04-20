bd2ca74f9cd2c62ebd43f4e8114514db8a4881ce
ClaeCarlson <ccarlson04@aurora.edu>
2018-04-20 09:44:22 -0500

    add sensor field to getLogs. Handling for null

--- php/getLogs.php
+++ php/getLogs.php
@@ -5,20 +5,40 @@ require 'header.php';
 $result = $mysqli->query("SELECT * FROM logs ORDER BY time DESC");
 
 
-echo '<tr><th>Status</th><th>Time</th><th>User</th></tr>';
+echo '<tr><th>Status</th><th>Time</th><th>Sensor</th><th>User</th></tr>';
 
 while($row = $result->fetch_assoc()){
 
     $time = $row['time'];
     $status = $row['status'];
     $uid = $row['users_id'];
+    $sensor = $row['sensors_id'];
 
     $message = "";
     $username = "";
+    $sensorName = "";
 
+    if ($sensor != NULL) {
+    $sensorResult = $mysqli->query("SELECT name FROM sensors WHERE id = '$sensor'");
+    $sensorName = $sensorResult->fetch_assoc();
+    $sensorName = $sensorName['name'];
+    }
+    else {
+        $sensorName = "N/A";
+    }
+
+    if ($uid != NULL) {
     $userResult = $mysqli->query("SELECT username FROM users WHERE id = '$uid'");
     $username = $userResult->fetch_assoc();
-    $username = $username['username'];
+    $username = $username['username']; 
+    }
+    else {
+        $username == "N/A";
+    }
+
+
+
+
 
     if ($status == 1) {
 
@@ -31,7 +51,7 @@ while($row = $result->fetch_assoc()){
 
     }
 
-    echo '<tr><td>' . $message . '</td><td>' . $time . '</td><td>' . $username . '</td></tr>';
+    echo '<tr><td>' . $message . '</td><td>' . $time . '</td><td>' . $sensorName . '</td><td>' . $username . '</td></tr>';
 
 }
 
